40fb5ac3a12fbe97bd1ce3ba068a3c4cb6558498
osho <oshosingh25@gmail.com>
2022-04-25 19:13:50 +0530

    added

---
+++ dp/dp3.java
@@ -0,0 +1,73 @@
+// Egg Drop
+
+int[][] dp = new int[n][k];
+int eggDrop(int n, int k){
+    if(k<=1){
+        return 0;
+    }
+    if(n==1){
+        return k;
+    }
+    if(dp[n][k] != -1){
+        return dp[n][k];
+    }
+
+    int x = 1;
+    int min = Integer.MAX_VALUE;
+
+    for(x=1; x<=k; x++){
+        int res = Math.max( eggDrop(n, k-x), eggDrop(n-1, x-1) );
+        min = Math.min(min, res);
+    }
+
+    dp[n][k] = min+1;  //add 1 because use one egg should be counted as trial
+    return dp[n][k];
+}
+
+// Maximum increasing subsequence
+int maxSum(int a[], int n){
+    int[] dp = new int[n];
+    int max = 0;
+
+    for(int i=0; i<n; i++){
+        dp[i] = a[i];
+    }
+
+    for(int i=n-2; i>=0; i--){
+        for(int j=i+1; j<n; j++){
+            if(a[i]<a[j]){
+                dp[i] = Math.max(dp[i], a[i]+dp[j]);
+            }
+        }
+        max = Math.max(max, dp[i]);
+    }
+
+    return max;
+}
+
+// Kadanes' algorithm. maximum subarray sum
+int kadane(int a[], int n){
+    int maxSoFar = 0;
+    int maxEndingHere = 0;
+    int start=0, end = 0, s=0;
+    for(int i=0; i<n; i++){
+        maxEndingHere += a[i];
+        if(maxSoFar < maxEndingHere){
+            maxSoFar = maxEndingHere;
+            start = s;
+            end = i;
+        }
+        else{
+            maxEndingHere = 0;
+            s = i+1;
+        }
+    }
+    // [a[start], a[end]] is the subarray 
+    return maxSoFar;
+}
+
+// Cut rod
+int cutRod(int price[], int n){
+    int[] dp = new int[n+1];
+    dp[0] = 0;
+}
