34468b636fc6c99b15c3045686620a89024117fe
osho <oshosingh25@gmail.com>
2022-04-28 13:17:31 +0530

    added

--- dp/dp3.java
+++ dp/dp3.java
@@ -81,3 +81,25 @@ int cutRod(int price[], int n){
 
     return dp[n];
 }
+
+// Min Cost Path
+int cost(int[][] grid){
+    int m = grid.length; int n = grid[0].length;
+    //first col
+    for(int i=1; i<m; i++){
+        grid[i][0] += grid[i-1][0];
+    }
+
+    //first row
+    for(int i=1; i<n; i++){
+        grid[0][i] += grid[0][i-1];
+    }
+
+    for(int i=1; i<m; i++){
+        for(int j=1; j<n; j++){
+            grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+        }
+    }
+
+    return grid[m-1][n-1];
+}
