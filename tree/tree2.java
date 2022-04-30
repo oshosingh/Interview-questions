a11698881f646ffaa4fb2e6d2e3a4b9ebf5797d9
osho <oshosingh25@gmail.com>
2022-04-30 13:32:39 +0530

    added

--- tree/tree2.java
+++ tree/tree2.java
@@ -7,8 +7,9 @@ TreeNode tree(List<Integer> pre, List<Integer> in){
         TreeNode root = new TreeNode(pre.get(0));
         int mid = in.indexOf(pre.get(0));
         
-        root.left = tree(pre.subList(1,mid+1), in.subList(0, mid));
-        root.right = tree(pre.subList(mid+1, pre.size()), in.subList(mid+1, in.size()));
+        root.left = tree(pre.subList(1,mid+1), in.subList(0, mid)); // [ pre(1, mid_1) in(0, mid) ]
+        root.right = tree(pre.subList(mid+1, pre.size()), in.subList(mid+1, in.size())); 
+        // [ pre(mid+1, size) in(mid+1, size) ]
         
         return root;
 }
@@ -22,8 +23,10 @@ TreeNode solve(List<Integer> in, List<Integer> post){
         int indRoot = in.indexOf(root.val);
         
         root.left = solve(in.subList(0, indRoot), post.subList(0, indRoot));
+        // [ in(0, ind) post(0, ind) ]
         
         root.right = solve(in.subList(indRoot+1, in.size()), post.subList(indRoot, in.size()-1));
-        
+        // [ in(ind+1, size) post(ind+1, size()-1) ]
+
         return root;
 }
