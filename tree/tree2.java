8034b1c5dcb3c694a42d45e7f862969389f972a9
osho <oshosingh25@gmail.com>
2022-04-30 13:30:05 +0530

    added

--- tree/tree2.java
+++ tree/tree2.java
@@ -1,20 +1,4 @@
 // Construct Binary Tree from Preorder and Inorder Traversal
-TreeNode buildTree(int[] preorder, int[] inorder) {
-       List<Integer> pre = new ArrayList<>();
-        
-        for(int x : preorder){
-            pre.add(x);
-        }
-        
-        List<Integer> in = new ArrayList<>();
-        
-        for(int x : inorder){
-            in.add(x);
-        }
-        
-        return tree(pre, in);
-}
-    
 TreeNode tree(List<Integer> pre, List<Integer> in){
         if(pre.size()==0 && in.size()==0){
             return null;
@@ -25,6 +9,21 @@ TreeNode tree(List<Integer> pre, List<Integer> in){
         
         root.left = tree(pre.subList(1,mid+1), in.subList(0, mid));
         root.right = tree(pre.subList(mid+1, pre.size()), in.subList(mid+1, in.size()));
+        
+        return root;
+}
+
+// Construct Binary Tree from Inorder and Postorder
+TreeNode solve(List<Integer> in, List<Integer> post){
+        if(in.size()==0 && post.size()==0){
+            return null;
+        }
+        TreeNode root = new TreeNode(post.get(post.size()-1));
+        int indRoot = in.indexOf(root.val);
+        
+        root.left = solve(in.subList(0, indRoot), post.subList(0, indRoot));
+        
+        root.right = solve(in.subList(indRoot+1, in.size()), post.subList(indRoot, in.size()-1));
         
         return root;
 }
