1da766e4c31d12c4cf93d2a2e6814a7e97f75e1e
osho <oshosingh25@gmail.com>
2022-05-05 22:59:44 +0530

    added

--- heap/heap.java
+++ heap/heap.java
@@ -20,6 +20,47 @@ int[] maxSubsequence(int[] nums, int k) {
     while(!q.isEmpty()){
         ans[i++] = q.poll().get(0);
     }
-        
     return ans;
+}
+
+// Largest Number After Digit Swaps by Parity
+/**
+    You are given a positive integer num. You may swap any two digits of num that have
+    the same parity (i.e. both odd digits or both even digits).
+    Return the largest possible value of num after any number of swaps.
+ */
+
+ int largestInteger(int num) {
+
+    // Max heap to store num's single digit integer
+    PriorityQueue<Integer> even = new PriorityQueue<>();
+    PriorityQueue<Integer> odd = new PriorityQueue<>();
+        
+    int temp = num;
+    String ans = "";
+        
+    while(num!=0){
+        int x = num%10;
+        if(x%2==1){
+                odd.add(x);
+        }
+        else{
+                even.add(x);
+        }
+        num/=10;
+    }
+    
+    // Loop over single digits and take values from heap based on parity
+    while(temp!=0){
+        int x = temp%10;
+        if(x%2==1){
+            ans = String.valueOf(odd.poll())+ans;
+        }
+        else{
+            ans = String.valueOf(even.poll())+ans;
+        }
+        temp /= 10;
+    }
+        
+    return Integer.parseInt(ans);
 }
