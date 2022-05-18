cb7aee116a746ea894221f167cb7facac4f856a6
osho singh <oshosingh25@gmail.com>
2022-05-18 11:25:42 +0000

    added

---
+++ notes/singleton.java
@@ -0,0 +1,12 @@
+class Singleton{
+    private static Singleton obj = null;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(obj==null)
+            obj = new Singleton();
+        
+        return obj;
+    }
+}
