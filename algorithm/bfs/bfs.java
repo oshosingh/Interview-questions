8bc407a8095f82f4a5bb6e2c3e33f7e2023f4452
osho singh <oshosingh25@gmail.com>
2022-06-05 17:07:50 +0000

    added

--- algorithm/bfs/bfs.java
+++ algorithm/bfs/bfs.java
@@ -37,3 +37,64 @@ public boolean isInLimit(int x, int y, int grid[][]){
     if(x<0||x>=grid.length||y<0||y>=grid[0].length||grid[x][y]==1)return false;
     return true;
 }
+
+==============================================================================================================================================
+
+// Given a binary matrix where 0 represents water and 1 represents land, and connected ones form an island, count the total islands.
+
+int countIslands(int[][] mat){
+
+    // Base condition
+    if(mat == null || mat.length==0) return 0;
+
+    int m = mat.length;
+    int n = mat[0].length;
+
+    int islands = 0;
+
+    boolean[][] visited = new boolean[m][n];
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            
+            // Do BFS
+            if(mat[i][j] == 1 && !visited[i][j]){
+                bfs(mat, i, j, visited);
+                islands++;
+            }
+        }
+    }
+}
+
+void bfs(int[][] mat, int i, int j, boolean[][] visited){
+    Queue<List<Integer>> q = new LinkedList<>();
+    q.add(Arrays.asList(i,j));
+
+    int[][] dir = new int[][] { {-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {-1,1}, {1,0}, {1,1} };
+
+    visited[i][j] = true;
+
+    while(!q.isEmpty()){
+        List<Integer> poll = q.poll();
+        int x = poll.get(0);
+        int y = poll.get(1);
+
+        for(int[] r : dir){
+            int dx = x + r[0];
+            int dy = y + r[1];
+
+            if(isSafe(dx, dy, visited, mat)){
+
+                visited[dx][dy] = true;
+                q.add(Arrays.asList(dx, dy));
+            }
+        }
+    }
+}
+
+boolean isSafe(int x. int y, boolean[][] visited, int[][] mat){
+    if(x<0 || x>mat.length-1 || y<0 || y>mat[0].length-1 || visited[x][y]) return false;
+    return true;
+}
+
+======================================================================================================================================
